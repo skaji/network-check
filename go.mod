b7994933191930b3bb9aec95663ff5bb39499398
Shoichi Kaji <skaji@cpan.org>
2024-10-25 07:37:27 +0900

    update

--- go.mod
+++ go.mod
@@ -1,20 +1,23 @@
 module github.com/skaji/network-check
 
-go 1.22.2
+go 1.23.2
 
 require (
-	github.com/jackpal/gateway v1.0.14
-	github.com/prometheus-community/pro-bing v0.4.0
+	github.com/jackpal/gateway v1.0.15
+	github.com/miekg/dns v1.1.62
+	github.com/prometheus-community/pro-bing v0.4.1
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/tools v0.26.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/jackpal/gateway"
+	"github.com/miekg/dns"
 	probing "github.com/prometheus-community/pro-bing"
 )
 
@@ -45,6 +47,7 @@ func main() {
 	ping(gw.String())
 	ping("8.8.8.8")
 	resolve("www.google.com")
+	resolve2("www.google.com")
 }
 
 func ping(addr string) {
@@ -60,14 +63,56 @@ func ping(addr string) {
 	fmt.Printf("OK ping %s (%s)\n", addr, stats.AvgRtt.String())
 }
 
-func resolve(addr string) {
+func resolve(domain string) {
 	var resolver *net.Resolver
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	ips, err := resolver.LookupIP(ctx, "ip", addr)
+	ips, err := resolver.LookupIP(ctx, "ip", domain)
 	if err != nil {
-		fmt.Printf("NG resolve %s (%v)\n", addr, err)
+		fmt.Printf("NG resolve from local (%v)\n", err)
 		return
 	}
-	fmt.Printf("OK resolve %s (%s)\n", addr, ips[0])
+	fmt.Printf("OK resolve from local (%s -> %s)\n", domain, ips[0])
+}
+
+func resolve2(domain string) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	client := &dns.Client{Timeout: time.Second}
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		msg := &dns.Msg{}
+		msg.SetQuestion(dns.Fqdn(domain), dns.TypeA)
+		dnsServer := "1.1.1.1:53"
+		res, _, err := client.ExchangeContext(ctx, msg, dnsServer)
+		if err != nil {
+			fmt.Printf("NG resolve from %s (%v)\n", dnsServer, err)
+			return
+		}
+		for _, answer := range res.Answer {
+			if a, ok := answer.(*dns.A); ok {
+				fmt.Printf("OK resolve from %s (%s -> %s)\n", dnsServer, domain, a.A.String())
+			}
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		msg := &dns.Msg{}
+		msg.SetQuestion(dns.Fqdn(domain), dns.TypeAAAA)
+		dnsServer := "[2606:4700:4700::1111]:53"
+		res, _, err := client.ExchangeContext(ctx, msg, dnsServer)
+		if err != nil {
+			fmt.Printf("NG resolve from %s (%v)\n", dnsServer, err)
+			return
+		}
+		for _, answer := range res.Answer {
+			if a, ok := answer.(*dns.AAAA); ok {
+				fmt.Printf("OK resolve from %s (%s -> %s)\n", dnsServer, domain, a.AAAA.String())
+			}
+		}
+	}()
+	wg.Wait()
 }
