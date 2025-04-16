66737c241803a0b4f999c5cbee6c170d9194a668
Shoichi Kaji <skaji@cpan.org>
2025-04-17 04:20:38 +0900

    update

--- main.go
+++ main.go
@@ -40,95 +40,68 @@ func main() {
 		fmt.Printf("cannot find local ip: %v\n", err)
 		os.Exit(1)
 	}
-	fmt.Printf("IP %s\n", myip)
-	fmt.Printf("GW %s\n", gw.String())
-	fmt.Println("")
+	fmt.Println("IP", myip)
+	fmt.Println("GW", gw.String())
 
-	ping("1.1.1.1", "2606:4700:4700::1111", gw.String())
-	udp("1.1.1.1:53", "[2606:4700:4700::1111]:53", gw.String()+":53")
-	tcp("1.1.1.1:443", "[2606:4700:4700::1111]:443", gw.String()+":80")
+	inputs := []Input{
+		{Type: "ping", Addr: "1.1.1.1"},
+		{Type: "ping", Addr: "2606:4700:4700::1111"},
+		{Type: "ping", Addr: gw.String()},
+		{Type: "tcp", Addr: "1.1.1.1:443"},
+		{Type: "tcp", Addr: "[2606:4700:4700::1111]:443"},
+		{Type: "tcp", Addr: gw.String() + ":80"},
+		{Type: "udp", Addr: "1.1.1.1:53"},
+		{Type: "udp", Addr: "[2606:4700:4700::1111]:53"},
+		{Type: "udp", Addr: gw.String() + ":53"},
+	}
+	errors := make([]error, len(inputs))
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	var wg sync.WaitGroup
+	wg.Add(len(inputs))
+	for i, input := range inputs {
+		go func() {
+			defer wg.Done()
+			errors[i] = Access(ctx, input)
+		}()
+	}
+	wg.Wait()
+
+	for i, input := range inputs {
+		ok := "\x1b[32mOK\x1b[m"
+		if err := errors[i]; err != nil {
+			ok = "\x1b[31mNG\x1b[m"
+		}
+		fmt.Println(ok, input.Type, input.Addr)
+	}
 }
 
-type result struct {
-	OK   bool
+type Input struct {
 	Type string
 	Addr string
 }
 
-func (r *result) String() string {
-	ok := "\x1b[32mOK\x1b[m"
-	if !r.OK {
-		ok = "\x1b[31mNG\x1b[m"
+func Access(ctx context.Context, input Input) error {
+	switch input.Type {
+	case "ping":
+		pinger := probing.New(input.Addr)
+		pinger.Count = 1
+		pinger.Timeout = time.Second
+		err := pinger.RunWithContext(ctx)
+		return err
+	case "tcp":
+		conn, err := (&net.Dialer{}).DialContext(ctx, "tcp", input.Addr)
+		if err == nil {
+			_ = conn.Close()
+		}
+		return err
+	case "udp":
+		msg := &dns.Msg{}
+		msg.SetQuestion(dns.Fqdn("www.google.com"), dns.TypeA)
+		_, _, err := (&dns.Client{}).ExchangeContext(ctx, msg, input.Addr)
+		return err
 	}
-	return fmt.Sprintf("%s %s %s", ok, r.Type, r.Addr)
-}
-
-func ping(addrs ...string) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	var wg sync.WaitGroup
-	wg.Add(len(addrs))
-	out := make([]*result, len(addrs))
-	for i, addr := range addrs {
-		go func() {
-			defer wg.Done()
-			pinger := probing.New(addr)
-			pinger.Count = 1
-			pinger.Timeout = time.Second
-			err := pinger.RunWithContext(ctx)
-			out[i] = &result{Type: "ping", OK: err == nil, Addr: addr}
-		}()
-	}
-	wg.Wait()
-	for _, str := range out {
-		fmt.Println(str)
-	}
-}
-
-func udp(addrs ...string) {
-	domain := "www.google.com"
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	client := &dns.Client{Timeout: time.Second}
-
-	var wg sync.WaitGroup
-	wg.Add(len(addrs))
-	out := make([]*result, len(addrs))
-	for i, addr := range addrs {
-		go func() {
-			defer wg.Done()
-			msg := &dns.Msg{}
-			msg.SetQuestion(dns.Fqdn(domain), dns.TypeA)
-			_, _, err := client.ExchangeContext(ctx, msg, addr)
-			out[i] = &result{Type: "udp", OK: err == nil, Addr: addr}
-		}()
-	}
-	wg.Wait()
-	for _, str := range out {
-		fmt.Println(str)
-	}
-}
-
-func tcp(addrs ...string) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	var wg sync.WaitGroup
-	wg.Add(len(addrs))
-	out := make([]*result, len(addrs))
-	for i, addr := range addrs {
-		go func() {
-			defer wg.Done()
-			conn, err := (&net.Dialer{}).DialContext(ctx, "tcp", addr)
-			if err == nil {
-				_ = conn.Close()
-			}
-			out[i] = &result{Type: "tcp", OK: err == nil, Addr: addr}
-		}()
-	}
-	wg.Wait()
-	for _, str := range out {
-		fmt.Println(str)
-	}
+	panic("unexpected")
 }
